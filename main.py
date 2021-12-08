55adb5c550b6aa8305f985b71031647aeb3593e7
Barun Ghosh <barungh@hotmail.com>
2021-12-08 07:44:03 +0530

    :fire: Removed comments
    Comments removed, typos corrected

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-# divident = int(input("Please enter divident :"))
-# divider = int(input("Enter divider :"))
-
-n = int(input("Please enterf a number to factor :"))
+n = int(input("Please enter a number to factorise :"))
 d = 2
 
-# factors = []
+factors = []
 
 def my_function(divident, divider):
     counter = 0
@@ -17,11 +14,7 @@ def my_function(divident, divider):
     quotient = counter
     remainder = divident
 
-    return quotient, remainder
-
-# result = my_function(divident, divider)
-
-# print(result[0])
+    return counter, remainder
 
 # We have a function named my_functions
 # which takes two arguments divident and divider
@@ -29,12 +22,6 @@ def my_function(divident, divider):
 
 factors = []
 
-# n = 84
-# d = 2
-
-# result_2 = my_function(n, d)
-
-# print(n)
 while (n != 1):
     if my_function(n, d)[1] ==0:
         n = my_function(n, d)[0]
@@ -44,49 +31,4 @@ while (n != 1):
         d = d +1
 
 print(factors)
-
-
-# if result_2[1]==0:
-#     factors.append(d)
-#     n = result_2[0]
-#     print(n)
-# else:
-#     d = d+1
-# 
-# print(n, d)
-# 
-# if result_2[1]==0:
-#     factors.append(d)
-#     n = result_2[0]
-#     print(n)
-# else:
-#     d = d+1
-# 
-#     
-# print(n, d)
-# print(factors)
-# result_debug = my_function(8820, 19)
-
-# print(result_debug)
-
-# while n != 1:
-#     result_debug = my_function(n, d)
-#     if result_debug[1]==0:
-#         print("It is a factor : ")
-#         n = result_debug[0]
-#         d = d+1
-#     else:
-#         print("Not a factor")
-    
-
-
-
-# for range in (2, n):
-#     divider = 2
-#     result = my_function(divider, n)
-#     if (result[1]==0):
-#         print("Divided by " + repr(divider))
-#         result[0] = n
-    
-
 
